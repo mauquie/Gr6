7945dc14c100875f18a7f679691b2c7ca1332144
Picke77 <71831346+Picke77@users.noreply.github.com>
2020-10-01 16:53:10 +0200

    Add files via upload

---
+++ NSI POKER controller v1.py
@@ -0,0 +1,94 @@
+#♣Lien regle: http://www.poker-texas-holdem.info/
+class PokerController :
+   
+ 
+    def la_cave(self):#Notre coffre a points,tous nos jetons
+        self.points=points
+        
+    def melange():#fais reapparaitre le paquets au complets
+        self.reset=reset
+     
+    def dealer():#il tourne toutes les manches les 2 
+          #joueurs sa gauche doiven,t miser obligatoirement
+          joueur=[1,2,3,4]
+          for i in range(0,3):
+              tab[0,i]=dealeur
+          
+   
+    def blind():#mise obligatoire du debut
+        petite blind 
+        grosse blind
+        #joueur qui debute a gauche de la grosse blind
+        
+    def pioche():#Piochez les 2 cartes du debut
+        
+    def Flop():#on decouvre les 3 cartes communes
+   
+    def Turn():#on decouvre 1 nouvelles cartes(4eme)
+    
+    def The_river():#♦on decouvre la 5eme carte
+        
+    def Reveler():#Toutes les cartes sont 
+        #reveler(nécessaire?)
+        
+    def winner():
+        
+        
+    def Fin():#arrete la partie au bout de 4 manches
+        
+                
+                
+        
+    
+        
+  
+        
+    
+        
+        
+class miser:#permet de miser
+    def call():#PERMET DE SUIVRE ET METTRE LA MEME MISE
+           
+       
+    def  Raise():#permet d'augmenter la mise 
+           a=int(input("De combien vous relancez"))
+      
+   def Fold():#permet de se retirer de la mise 
+           main.pop
+           Joueur.pop
+           
+   def Fin_tour1():#Fin quand sa reviens a grosse blind 
+       #sans relance
+       
+          
+   def surlancer(): #nécessaire 
+       
+
+       
+class miser2(miser):
+    
+    def checker:
+        self.regarder
+       
+#ephemere,je vais modifier   
+[  
+cave()
+melange()
+dealer()
+blind()
+pioche()
+miser()
+Flop()
+miser2()
+Turn()
+miser2()
+The_river()
+miser2()
+Reveler()
+winner()
+Fin()
+]
+           
+    
+
+    
